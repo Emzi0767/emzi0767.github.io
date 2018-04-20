365818e443e78aeeec3a223cb5e043ceaa4a55d6
Mateusz Brawański <emzi0767@gmail.com>
2018-04-20 09:11:20 +0000

    Update stegman docs.

--- stegman/search/all_b.js
+++ stegman/search/all_b.js
@@ -8,5 +8,6 @@ var searchData=
   ['print_5fusage',['print_usage',['../defs_8h.html#aa4c888ac08148b1a8427491238530dc7',1,'program.c']]],
   ['program_5fauthor',['PROGRAM_AUTHOR',['../defs_8h.html#a0030baa957dd71e646553e08cbfa1560',1,'program.c']]],
   ['program_5fdescription',['PROGRAM_DESCRIPTION',['../defs_8h.html#a3876eb59c0b18d9c47972da848cf76e8',1,'program.c']]],
+  ['program_5fname',['PROGRAM_NAME',['../defs_8h.html#ad04f1b2d3966f7c0d005b854487a5742',1,'program.c']]],
   ['program_5fversion',['PROGRAM_VERSION',['../defs_8h.html#a6fcc1b69f64758f9408e398a4666954d',1,'program.c']]]
 ];
